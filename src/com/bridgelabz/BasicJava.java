315403132704eb6a83fdab33f8ccccc365f3e709
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-28 16:44:50 +0530

    Added: the class revision

--- src/com/bridgelabz/BasicJava.java
+++ src/com/bridgelabz/BasicJava.java
@@ -1,5 +1,45 @@
 package com.bridgelabz;
-
 public class BasicJava {
+	public static int addTwo(int a,int b)
+	{
+		int sum=a+b;
+		return sum;
+	}
+	
+	public static void main(String args[]) {
+		int a=5;
+		int b=2;
+		int sum=0;
+		sum=addTwo(a,b);
+		System.out.println("sum of two number"+sum);
+		System.out.println("-----------------NEXT Question------------");
+		System.out.println("hello");
+		System.out.println("asjad");
+		if(a!=b) {
+			System.out.println(a+"!="+b+"=");
+			System.out.println(a!=b);
+		}
+		else {
+			System.out.print(+a+"!="+b+"="+(a!=b));
+			
+		}
+		if(a<b) {
+			System.out.print(+a+"<"+b+"=");
+			System.out.println(a<b);
+		}
+		else {
+			System.out.print(+a+"<"+b+"=");
+			System.out.println(a<b);
+		}
+		if(a>b) {
+			System.out.print(+a+">"+b+"=");
+			System.out.println(a<b);
+		}
+		else {
+			System.out.print(+a+">"+b+"=");
+			System.out.println(a>b);
+		}
+}
+	
 
 }
